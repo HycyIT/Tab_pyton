6f4cc19e20e99d241a3c74ad17871775766a8ca2
Tester Oprogramowania <testeroprogramowaniakontakt@gmail.com>
2024-03-05 15:22:59 +0100

    rename

--- calculator.py
+++ calculator.py
@@ -1,34 +1,20 @@
-def calculator():
-    
-        print('Wybierz 1, aby policzyć pole prostokąta')
-        print('Wybierz 2, aby policzyć pole kwadratu')
-        print('Wybierz 3, aby policzyć pole trójkąta')
-        print('Wybierz 4, aby policzyć pole trapezu')
-        print('Wybierz 5, aby policzyć pole koła')
+table = [-5,-2,-1,0,1,2,3,4,5,6,7,8]
+list = []
 
-        num = int(input('Wybierz, co chcesz zrobić: '))
-        
-        if num == 1:
-            a = int(input("Podaj 'a': "))
-            b = int(input("Podaj 'b': "))
-            return a * b
-        elif num == 2:
-            a = int(input("Podaj 'a': ")) 
-            return a * a
-        elif num == 3:
-            a = int(input("Podaj 'a': "))
-            h = int(input("Podaj 'h': "))
-            return (a * h) / 2
-        elif num == 4:
-            a = int(input("Podaj 'a': "))
-            b = int(input("Podaj 'b': "))
-            h = int(input("Podaj 'h': "))
-            return (a + b) * h / 2
-        elif num == 5:
-            r = int(input("Podaj 'r': "))
-            return 3.14 * r ** 2
-        else:
-            print('Niepoprawny wybór. Spróbuj ponownie.')
+def sumAll(table):
+    for i in table:
+        if i > 0 :
+           list.append(i)
+           newList = sum(list)
+    print(newList)
 
-wynik = calculator()
-print('Wynik: ', wynik)
+sumAll(table)
+
+
+
+def sum_positives(numbers):
+    return sum([number for number in numbers if number > 0])
+ 
+numbers = [-5,-2,-1,0,1,2,3,4,5,6,7,8]
+print(sum_positives(numbers)) 
+
